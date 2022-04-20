81b31f811e5ce8003f846728b4d01b6822b3324f
GabZzR <gabrielrosso609@gmail.com>
2022-04-20 16:55:10 +0200

    last commit

--- src/components/MagicColl.vue
+++ src/components/MagicColl.vue
@@ -10,13 +10,21 @@
               <header class="major">
                 <h2>Ma Collection</h2>
               </header>
-              <div class="card-list">
-                <div class="card-title"><b>#1</b> Card Title</div>
+              <!--<tbody>
+          <tr v-for="movie in movies" :key="movie.id">
+              <td>{{movie.id}}</td>
+              <td>{{movie.title}}</td>
+              <td>{{movie.release_date}}</td>
+              <td @click="goToFilmsDetails(movie.id)">👀</td>
+          </tr>
+              </tbody>
+              <div class="card-list" v-for="card in cards" :key="card.id">
+                <div class="card-title"><b>{{cards.id}}</b> Card Title</div>
                 <div class="card-action"><a href="#">👀</a></div>
-              </div>
-              <div class="card-list">
-                <div class="card-title"><b>#2</b> Card Title</div>
-                <div class="card-action"><a href="#">👀</a></div>
+              </div> -->
+              <div class="card-list" v-for="card in magic" :key="card.id">
+                <div class="card-title"><b>{{card.id}}</b> {{card.name}}</div>
+                <div class="card-action"><a>👀</a></div>
               </div>
             </article>
           </div>
@@ -27,8 +35,23 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "MagicColl",
+  data:()=>({
+    magic:[]
+}),
+methods:{
+    goToCardsDetails(cardsId){
+        this.$router.push({name: 'cardDetails' , params: {cardsId}})
+    }
+},
+async created(){
+let magic = await axios.get("https://api.magicthegathering.io/v1/cards")
+this.magic = magic.data
+console.log('CREATED');
+}
 };
 </script>
 
